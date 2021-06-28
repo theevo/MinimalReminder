5030398f229f1966e8ef4150d0be68b7b400c948
Theo Vora <theo.vora@gmail.com>
2021-06-28 15:25:37 -0600

    Add UserDefaults

---
+++ MinimalReminder/MRUserDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  MRUserDefaults.swift
+//  MinimalReminder
+//
+//  Created by Theo Vora on 6/28/21.
+//
+
+import Foundation
+
+struct MRUserDefaults {
+    static let userDefaults = UserDefaults.standard
+    static let userAllowedNotifications = false
+}
